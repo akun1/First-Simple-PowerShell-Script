6f6dbb33412869109f4e37f557db6311fbd959ea
Akash Kundu <akashkundu92@gmail.com>
2017-04-03 03:54:31 -0500

    Add files via upload

---
+++ startup.ps1
@@ -0,0 +1,2 @@
+﻿Start-Process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList 'gmail.com', 'ecampus.tamu.edu', 'piazza.com','http://courses.cse.tamu.edu/yum/Spring2017/312/', 'http://robotics.cs.tamu.edu/dshell/cs314/'
+
